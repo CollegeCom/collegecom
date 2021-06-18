f742753575e7f9a19497ffce283cbca0b677d32e
Nayan Arora <nayanarora55@gmail.com>
2021-06-18 23:53:17 +0530

    Added Landing Page

--- IISTtoday/views.py
+++ IISTtoday/views.py
@@ -7,14 +7,17 @@ from notice.models import Notice
 from accounts.models import extUser
 from feed.models import Feed
 
-@login_required(login_url='login')
+# @login_required(login_url='login')
 def home(request):
-    feed=Feed.objects.order_by('-id')
-    events=Event.objects.all()
-    notices=Notice.objects.all()
-    user=extUser.objects.get(user__id=request.user.id)
-    params={"euser":user,'feed':feed,'events':events[::-1][:3],'notices':notices[::-1][:3]}
-    return render(request,'dashboard.html',params)
+    if request.user.is_authenticated:
+        feed=Feed.objects.order_by('-id')
+        events=Event.objects.all()
+        notices=Notice.objects.all()
+        user=extUser.objects.get(user__id=request.user.id)
+        params={"euser":user,'feed':feed,'events':events[::-1][:3],'notices':notices[::-1][:3]}
+        return render(request,'dashboard.html',params)
+    else:
+        return render(request,'home.html')
 
 def error404(request):
     return render(request,'404-error.html')
